b8e7cc8a421c4fa30f14f57e6fdfb829f20bba06
yongjae5717 <yjk@YJui-MacBookPro.local>
2022-08-31 00:05:10 +0900

    using Servlet with MVC

---
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
@@ -0,0 +1,34 @@
+package hello.servlet.web.servletmvc;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "mvcMemberFormServlet", urlPatterns = "/servlet-mvc/members/new-form")
+public class MvcMemberFormServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        /**
+         * WEB-INF: 이 경로안에 JSP가 있으면 외부에서 직접 JSP를 호출할 수 없다. 우리가 기대하는 것은 항상 컨트롤러를
+         * 통해서 JSP를 호출하는 것이다.
+         */
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        //서블릿에서 JSP호출
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+
+        //다른 서블릿이나 JSP로 이동할 수 있는 기능이다.
+        dispatcher.forward(request, response); //서버끼리 내부에서 호출
+
+        /**
+         * redirect vs forward
+         *  리다이렉트는 실제 클라이언트(웹 브라우저)에 응답이 나갔다가, 클라이언트가 redirect 경로로 다시 요청한다.
+         *  따라서 클라이언트가 인지할 수 있고, URL 경로도 실제로 변경된다.
+         *  반면에 포워드는 서버 내부에서 일어나는 호출이기 때문에 클라이언트가 전혀 인지하지 못한다.
+         */
+    }
+}
